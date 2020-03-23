5b0717afdd0b4e92de9930d70a782d2507f7a37e
Traci Crawford <tracicrawford@tracis-air.attlocal.net>
2020-03-23 14:48:32 -0500

    Higher Order Components Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExchangedCurrency from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2>Higher Order Component</h2>
+        <ExchangedCurrency />
       </div>
     );
   }
